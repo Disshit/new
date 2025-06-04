953c50f2dfbcba35612f6b3be0f85bc9b6a7c891
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2025-06-04 14:57:59 +0600

    20250504145756

---
+++ scripts/theme.js
@@ -0,0 +1,61 @@
+/* Light theme should be updated later. Delete this when done. */
+
+const darkTheme = `
+    html, html[data-dark="default"] {
+        --accent-color: #f29c24;
+        --text-color: #f8fafc;
+        --bg-deep: #202124;
+        --bg-mid: #28292a;
+        --bg-lite: #2d2f31;
+    }
+`;
+
+const lightTheme = `
+    html, html[data-light="default"] {
+        --accent-color: #f29c24;
+        --text-color: #080a0c;
+        --bg-deep: #ffffff;
+        --bg-mid: #f9f9f9;
+        --bg-lite: #f2f2f3;
+    }
+`;
+
+// Add Colors to CSS
+function addColors() {
+    let nightCSS = document.getElementById('nightCSS'),
+        dayCSS = document.getElementById('dayCSS');
+    
+    nightCSS.textContent = darkTheme.replace(/\n/g,'').replace(/[\t\s]/g,'');
+    dayCSS.textContent = lightTheme.replace(/\n/g,'').replace(/[\t\s]/g,'');
+}
+
+// Switch Themes
+function switchColors(key) {
+    let nightCSS = document.getElementById('nightCSS'),
+        dayCSS = document.getElementById('dayCSS');
+
+    let mediaNight = '(prefers-color-scheme: no-preference), (prefers-color-scheme: dark)',
+        mediaDay = '(prefers-color-scheme: light)',
+        typeEnabled = 'text/css',
+        typeDisabled = 'null';
+
+    if (key === 'day') {
+        nightCSS.setAttribute('type',typeDisabled);
+        dayCSS.setAttribute('type',typeEnabled);
+        nightCSS.removeAttribute('media');
+        dayCSS.removeAttribute('media');
+    } else if (key === 'night') {
+        nightCSS.setAttribute('type',typeEnabled);
+        dayCSS.setAttribute('type',typeDisabled);
+        nightCSS.removeAttribute('media');
+        dayCSS.removeAttribute('media');
+    } else {
+        nightCSS.setAttribute('type',typeEnabled);
+        dayCSS.setAttribute('type',typeEnabled);
+        nightCSS.setAttribute('media',mediaNight);
+        dayCSS.setAttribute('media',mediaDay);
+    }
+}
+
+addColors();
+switchColors(localStorage.switchState);
