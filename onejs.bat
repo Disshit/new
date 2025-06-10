b3a4df6a18385d641c4750605bc17e60c7719149
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2025-06-11 02:58:03 +0600

    20250511025755

--- onejs.bat
+++ onejs.bat
@@ -1,30 +1,14 @@
 :: Compile all scripts into One.
 @echo off
-setlocal
 
 set "cdir=%~dp0"
 
-set "outputMAX=%cdir%scripts\one.js"
-if exist "%outputMAX%" del "%outputMAX%"
-set "inputMAX=%cdir%scripts\oneJS\*.js"
+set "output=%cdir%scripts\one.min.js"
+if exist "%output%" del "%output%"
+set "input=%cdir%scripts\oneJS\*.min.js"
 
-set "outputMIN=%cdir%scripts\one.min.js"
-if exist "%outputMIN%" del "%outputMIN%"
-set "inputMIN=%cdir%scripts\oneJS\*.min.js"
-
-for %%f in ("%inputMAX%") do (
+for %%f in ("%input%") do (
     if exist "%%f" (
-        (   type "%%f"
-            echo.
-            echo.
-        ) >> "%outputMAX%"
+        type "%%f" >> "%output%"
     )
 )
-
-for %%f in ("%inputMIN%") do (
-    if exist "%%f" (
-        type "%%f" >> "%outputMIN%"
-    )
-)
-
-endlocal
