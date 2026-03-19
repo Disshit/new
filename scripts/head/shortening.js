71afa217f4f6f7e2442774ea3d9eaf41724de02a
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2026-03-20 03:36:23 +0600

    20260220033614

--- scripts/head/shortening.js
+++ scripts/head/shortening.js
@@ -3,23 +3,23 @@
  */
 
 const shortening = {
-    isgd: async function (link) {
-        const url = 'https://is.gd/create.php?format=json&url=' + encodeURIComponent(link.trim()),
+    isgd: async function (longurl) {
+        const url = 'https://is.gd/create.php?format=json&url=' + encodeURIComponent(longurl.trim()),
               response = await fetch(url),
               data = await response.json();
 
         // console.log(data.shorturl);
         return data.shorturl;
     },
-    vgd: async function (link) {
-        const url = 'https://v.gd/create.php?format=json&url=' + encodeURIComponent(link.trim()),
+    vgd: async function (longurl) {
+        const url = 'https://v.gd/create.php?format=json&url=' + encodeURIComponent(longurl.trim()),
               response = await fetch(url),
               data = await response.json();
 
         // console.log(data.shorturl);
         return data.shorturl;
     },
-    spoome: async function (link) {
+    spoome: async function (longurl) {
         const url = 'https://spoo.me/',
               options = {
                   method: 'POST',
@@ -28,12 +28,27 @@ const shortening = {
                       'Content-Type': 'application/x-www-form-urlencoded'
                   },
                   // body: new URLSearchParams({url: 'https://example.com/'})
-                  body: new URLSearchParams({url: link.trim()})
+                  body: new URLSearchParams({url: longurl.trim()})
               },
               response = await fetch(url, options),
               data = await response.json();
         
         // console.log(data.short_url);
         return data.short_url;
+    },
+    trimmlink: async function (longurl) {
+        const url = "https://trimm.link/",
+              response = await fetch(url + 'shorten', {
+                  method: 'POST',
+                  headers: {
+                      'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({link: longurl})
+              }),
+              json = await response.json(),
+              shorturl = url + (json.word || "");
+
+        // console.log(shorturl);
+        return shorturl;
     }
 };
